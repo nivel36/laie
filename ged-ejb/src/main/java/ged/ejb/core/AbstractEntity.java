87c80e67b90762831d2aa217cf220d81e858c4bd
Abel Ferrer <igochan@gmail.com>
2016-01-22 19:55:30 +0100

    Añadida la entidad User

---
+++ ged-ejb/src/main/java/ged/ejb/core/AbstractEntity.java
@@ -0,0 +1,36 @@
+package ged.ejb.core;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
+
+@MappedSuperclass
+public class AbstractEntity {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Version
+	private long version;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getVersion() {
+		return version;
+	}
+
+	public void setVersion(long version) {
+		this.version = version;
+	}
+	
+	
+
+}
