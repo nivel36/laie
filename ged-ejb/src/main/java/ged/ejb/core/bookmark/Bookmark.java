d87489b3f621a904c5c45cea18645ce79ccf9f00
igochan <igochan@gmail.com>
2016-09-17 13:02:27 +0200

    Bug en equals

--- ged-ejb/src/main/java/ged/ejb/core/bookmark/Bookmark.java
+++ ged-ejb/src/main/java/ged/ejb/core/bookmark/Bookmark.java
@@ -32,6 +32,9 @@ public class Bookmark extends AbstractEntity {
 
 	@Override
 	public boolean equals(final Object obj) {
+		if (obj == null) {
+			return false;
+		}
 		if (this == obj) {
 			return true;
 		}
