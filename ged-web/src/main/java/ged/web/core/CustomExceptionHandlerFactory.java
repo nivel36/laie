80c5e81255a79078f8c0ea3c2dd7d787bf3b180a
igochan <igochan@gmail.com>
2017-01-19 14:45:21 +0100

    Exception factory

---
+++ ged-web/src/main/java/ged/web/core/CustomExceptionHandlerFactory.java
@@ -0,0 +1,18 @@
+package ged.web.core;
+
+import javax.faces.context.ExceptionHandler;
+import javax.faces.context.ExceptionHandlerFactory;
+
+public class CustomExceptionHandlerFactory extends ExceptionHandlerFactory {
+
+	private final ExceptionHandlerFactory parent;
+
+	public CustomExceptionHandlerFactory(final ExceptionHandlerFactory parent) {
+		this.parent = parent;
+	}
+
+	@Override
+	public ExceptionHandler getExceptionHandler() {
+		return new ApplicationExceptionHandler(this.parent.getExceptionHandler());
+	}
+}
