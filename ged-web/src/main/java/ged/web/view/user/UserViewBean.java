3952d6108b5f39236715604b4ed430734b56795e
igochan <igochan@gmail.com>
2017-08-23 21:07:36 +0200

    User view

--- ged-web/src/main/java/ged/web/view/user/UserViewBean.java
+++ ged-web/src/main/java/ged/web/view/user/UserViewBean.java
@@ -4,18 +4,27 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.application.NavigationHandler;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.omnifaces.util.Faces;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import ged.ejb.job.offer.JobOffer;
 import ged.ejb.job.offer.JobOfferService;
 import ged.ejb.user.User;
 import ged.ejb.user.UserService;
+import ged.ejb.user.role.Role;
+import ged.ejb.user.role.RoleService;
 import ged.web.core.util.MessageUtils;
+import ged.web.core.util.TransaltionUtils;
 import ged.web.core.view.AbstractPageBean;
 import ged.web.reports.UserReport;
 
@@ -23,11 +32,19 @@ import ged.web.reports.UserReport;
 @ViewScoped
 public class UserViewBean extends AbstractPageBean {
 
+	protected static final transient Logger logger = LoggerFactory.getLogger(UserViewBean.class.getName());
+
 	private static final long serialVersionUID = -2187385732087309689L;
+
+	private boolean editable;
 
 	private List<JobOffer> jobOffers;
 
 	private final transient JobOfferService jobOfferService;
+
+	private User manager;
+
+	private final transient RoleService roleService;
 
 	private List<User> team;
 
@@ -38,18 +55,22 @@ public class UserViewBean extends AbstractPageBean {
 	private final transient UserService userService;
 
 	@Inject
-	public UserViewBean(final UserService userService, final JobOfferService jobOfferService) {
+	public UserViewBean(final UserService userService, final JobOfferService jobOfferService,
+			final RoleService roleService) {
 		Objects.requireNonNull(userService);
 		Objects.requireNonNull(jobOfferService);
+		Objects.requireNonNull(roleService);
 		this.userService = userService;
 		this.jobOfferService = jobOfferService;
+		this.roleService = roleService;
 	}
 
-	public String editUser() {
-		this.flash.put("user", this.user);
-		final String returnAddress = "userView.xhtml?id" + this.user.getId();
-		this.flash.put("returnAddress", returnAddress);
-		return "userEdit?faces-redirect=true";
+	public void cancelEditUser() {
+		this.editable = false;
+	}
+
+	public void editUser() {
+		this.editable = true;
 	}
 
 	private void error() {
@@ -65,6 +86,10 @@ public class UserViewBean extends AbstractPageBean {
 
 	public List<JobOffer> getJobOffers() {
 		return this.jobOffers;
+	}
+
+	public User getManager() {
+		return this.manager;
 	}
 
 	public List<User> getTeam() {
@@ -96,16 +121,56 @@ public class UserViewBean extends AbstractPageBean {
 		if (this.user == null) {
 			error();
 		}
-		this.team =  this.userService.findSubordinateUsers(id);
-		this.jobOffers =  this.jobOfferService.findAllByOwner(this.user);
+		this.team = this.userService.findSubordinateUsers(id);
+		this.jobOffers = this.jobOfferService.findAllByOwner(this.user);
 		if (this.user.isDeleted()) {
 			MessageUtils.addWarningMessage("message.erased_entity", "message.erased_entity");
 		}
 	}
 
+	private boolean isAvalidRole(final Role userRole, final Role managerRole) {
+		if (userRole.getName().equals(managerRole.getName())) {
+			return true;
+		}
+		return this.roleService.isASubordinateRole(managerRole, userRole);
+	}
+
+	public boolean isEditable() {
+		return this.editable;
+	}
+
 	public String modifyUser() {
 		this.flash.put("user", this.user);
 		return "userEdit?faces-redirect=true";
+	}
+
+	private void newManager() {
+		this.manager = new User();
+		this.manager.setName("");
+		this.manager.setSurename("");
+	}
+
+	public void removeManager() {
+		logger.debug("Remove manager action performed");
+		newManager();
+		this.user.setManager(null);
+	}
+
+	public void saveUser() {
+		logger.debug("Save user action performed");
+		setManager();
+		this.userService.save(this.user);
+	}
+
+	private void setManager() {
+		if (this.manager.getUsername() != null) {
+			logger.trace("The user has no manager");
+			this.user.setManager(this.manager);
+		}
+	}
+
+	public void setManager(final User manager) {
+		this.manager = manager;
 	}
 
 	public void setUser(final User user) {
@@ -118,5 +183,71 @@ public class UserViewBean extends AbstractPageBean {
 
 	public void undeleteUser() {
 		this.userService.undelete(this.user);
+	}
+
+	public void validateEmail(final FacesContext context, final UIComponent component, final Object value)
+			throws ValidatorException {
+		logger.debug("Checking email");
+		if (value == null) {
+			return;
+		}
+		final String email = (String) value;
+		if (value.equals(this.user.getEmail())) {
+			// Si el valor del email es el mismo que el que estamos validando
+			// es porque estamos actualizando un valor (que no es el email)
+			// y no hace falta que validemos si el registro existe (que oor otra
+			// parte sí lo estará)
+			return;
+		}
+		if (this.userService.emailExists(email)) {
+			logger.debug("The email exists");
+			final String msg = TransaltionUtils.translate("user.error.email_exists");
+			throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, msg));
+		}
+	}
+
+	public void validateManager(final FacesContext context, final UIComponent component, final Object value)
+			throws ValidatorException {
+		if (value == null) {
+			return;
+		}
+		if (this.manager.equals(this.user)) {
+			logger.debug("User can't be his/her manager");
+			final String msg = TransaltionUtils.translate("user.error.manager");
+			throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, msg));
+		}
+	}
+
+	public void validateRole(final FacesContext context, final UIComponent component, final Object value)
+			throws ValidatorException {
+		if (this.manager.getUsername() == null) {
+			return;
+		}
+		final Role userRole = (Role) value;
+		final Role managerRole = this.manager.getRole();
+		if (!isAvalidRole(userRole, managerRole)) {
+			final String msg = TransaltionUtils.translate("user.error.role");
+			throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, msg));
+		}
+	}
+
+	public void validateUsername(final FacesContext context, final UIComponent component, final Object value)
+			throws ValidatorException {
+		if (value == null) {
+			return;
+		}
+		final String username = (String) value;
+		if (value.equals(this.user.getUsername())) {
+			// Si el valor del usuario es el mismo que el que estamos validando
+			// es porque estamos actualizando un valor (que no es el de usuario)
+			// y no hace falta que validemos si el registro existe (que por otra
+			// parte sí lo estará)
+			return;
+		}
+		if (this.userService.usernameExists(username)) {
+			logger.debug("The username exists");
+			final String msg = TransaltionUtils.translate("user.error.username_exists");
+			throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, msg));
+		}
 	}
 }
