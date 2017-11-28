7db7e885339ce8c2edba298e3f4ef66caa43f6df
aferrer <aferrer@Grijota.gfi.es>
2017-11-28 09:30:28 +0100

    Save user rest

--- ged-rest/src/main/java/ged/rest/user/UserRestController.java
+++ ged-rest/src/main/java/ged/rest/user/UserRestController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -13,10 +14,15 @@ import javax.ws.rs.core.MediaType;
 
 import ged.ejb.user.User;
 import ged.ejb.user.UserService;
+import ged.ejb.user.role.Role;
+import ged.ejb.user.role.RoleService;
 
 @Path("users")
 @ApplicationScoped
 public class UserRestController {
+
+	@Inject
+	private RoleService roleService;
 
 	@Inject
 	private UserService userService;
@@ -28,6 +34,28 @@ public class UserRestController {
 			userDtos.add(userDto);
 		}
 		return userDtos;
+	}
+
+	private User convertUserDtoToUser(final UserDto userDto) {
+		final User user = new User();
+		user.setDateOfJoin(userDto.getDateOfJoin());
+		user.setEmail(userDto.getEmail());
+		user.setImageFileName(userDto.getImageFileName());
+		user.setLanguage(userDto.getLanguage());
+		if (user.getManager() != null) {
+			final User manager = this.userService.findUserByEmail(userDto.getManagerEmail());
+			user.setManager(manager);
+		}
+		user.setName(userDto.getName());
+		user.setPhoneNumber(userDto.getPhoneNumber());
+		final List<Role> roles = this.roleService.findAllRoles();
+		for (final Role role : roles) {
+			if (role.getName().equals(userDto.getRoleName())) {
+				user.setRole(role);
+			}
+		}
+		user.setSurename(userDto.getSurename());
+		return user;
 	}
 
 	@GET
@@ -46,5 +74,14 @@ public class UserRestController {
 		final List<User> users = this.userService.findAll();
 		final List<UserDto> userDtos = this.convertToUserDtoList(users);
 		return userDtos;
+	}
+
+	@POST
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public UserDto insert(final UserDto userDto) {
+		final User user = this.convertUserDtoToUser(userDto);
+		final User savedUser = this.userService.save(user);
+		return new UserDto(savedUser);
 	}
 }
