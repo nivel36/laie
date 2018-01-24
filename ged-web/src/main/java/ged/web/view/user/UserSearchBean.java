a9b99568428a1fb9666618f1a1b4b1b45eff34d9
igochan <igochan@gmail.com>
2018-01-24 20:59:02 +0100

    UserSearch igualada a CandidateSearch

--- ged-web/src/main/java/ged/web/view/user/UserSearchBean.java
+++ ged-web/src/main/java/ged/web/view/user/UserSearchBean.java
@@ -13,10 +13,13 @@ import org.slf4j.LoggerFactory;
 
 import ged.ejb.core.action.Action;
 import ged.ejb.core.action.ActionService;
+import ged.ejb.user.User;
+import ged.ejb.user.UserService;
+import ged.web.core.view.AbstractPageBean;
 
 @Named
 @ViewScoped
-public class UserSearchBean extends AbstractUserSearchBean {
+public class UserSearchBean extends AbstractPageBean {
 
 	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass().getName());
 
@@ -30,6 +33,21 @@ public class UserSearchBean extends AbstractUserSearchBean {
 	private long numberOfUsersOfflineLastMonth;
 
 	private long numberOfUsersOnlineLastWeek;
+
+	private String searchText;
+
+	protected List<User> users;
+
+	@Inject
+	protected transient UserService userService;
+
+	private List<User> usersOnlineLastWeek;
+
+	public void clean() {
+		logger.debug("Cleaning search fields");
+		this.searchText = null;
+		search();
+	}
 
 	public void findUsersOffline() {
 		this.users = this.userService.findUsersOfflineLastMonth();
@@ -58,11 +76,24 @@ public class UserSearchBean extends AbstractUserSearchBean {
 		return this.numberOfUsersOnlineLastWeek;
 	}
 
+	public String getSearchText() {
+		return this.searchText;
+	}
+
+	public List<User> getUsers() {
+		return this.users;
+	}
+
+	public List<User> getUsersOnlineLastWeek() {
+		return this.usersOnlineLastWeek;
+	}
+
 	@PostConstruct
 	public void init() {
 		logger.trace("Init UserSearchBean");
 		search();
 		this.actions = this.actionService.findLastActions();
+		this.usersOnlineLastWeek = this.userService.findUsersOnlineLastWeek();
 		this.numberOfUsersOfflineLastMonth = this.userService.numberOfUsersOfflineLastMonth();
 		this.numberOfUsersOnlineLastWeek = this.userService.numberOfUsersOnlineLastWeek();
 	}
@@ -72,7 +103,28 @@ public class UserSearchBean extends AbstractUserSearchBean {
 		return "userEdit?faces-redirect=true";
 	}
 
+	public void search() {
+		logger.debug("Searching for users");
+		if (this.searchText == null) {
+			this.users = this.userService.findAll();
+		} else {
+			this.users = this.userService.search(this.searchText);
+		}
+	}
+
 	public void setActionService(final ActionService actionService) {
 		this.actionService = actionService;
+	}
+
+	public void setSearchText(final String searchText) {
+		this.searchText = searchText;
+	}
+
+	public void setUsers(final List<User> users) {
+		this.users = users;
+	}
+
+	public void setUserService(final UserService userService) {
+		this.userService = userService;
 	}
 }
