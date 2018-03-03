15820c06b5460710af5a13fbe6958d6a85f69123
igochan <igochan@gmail.com>
2018-03-03 11:33:03 +0100

    New client popup

---
+++ ged-web/src/main/java/ged/web/view/client/ClientEditBean.java
@@ -0,0 +1,50 @@
+package ged.web.view.client;
+
+import static ged.ejb.core.util.Parameters.map;
+import static ged.web.core.util.Page.CLIENT;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import ged.ejb.client.Client;
+import ged.ejb.client.ClientService;
+import ged.ejb.core.Address;
+import ged.web.core.util.Navigate;
+import ged.web.core.view.AbstractBean;
+
+@Named
+@ViewScoped
+public class ClientEditBean extends AbstractBean {
+
+	private static final long serialVersionUID = 1412905869664752048L;
+
+	private Client client;
+
+	@Inject
+	private transient ClientService clientService;
+
+	public Client buildNewClient() {
+		final Client newClient = new Client();
+		newClient.setOwner(this.sessionBean.getUser());
+		newClient.setAddress(new Address());
+		return newClient;
+	}
+
+	public Client getClient() {
+		return this.client;
+	}
+
+	public void init() {
+		this.client = buildNewClient();
+	}
+
+	public void onCloseDialog() {
+		this.client = null;
+	}
+
+	public void save() {
+		this.clientService.insert(this.client);
+		Navigate.to(CLIENT).withParams(map("id", this.client.getId())).doGet();
+	}
+}
