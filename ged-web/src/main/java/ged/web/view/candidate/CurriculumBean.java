12d48ddda720d64157d1416350e7c81be2f62d89
igochan <igochan@gmail.com>
2018-07-26 17:48:45 +0200

    Bug en curriculum

---
+++ ged-web/src/main/java/ged/web/view/candidate/CurriculumBean.java
@@ -0,0 +1,85 @@
+package ged.web.view.candidate;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import ged.ejb.candidate.Candidate;
+import ged.ejb.curriculum.Curriculum;
+import ged.ejb.curriculum.Education;
+import ged.ejb.curriculum.JobExperience;
+import ged.ejb.curriculum.Language;
+import ged.ejb.curriculum.Skill;
+import ged.web.core.view.AbstractBean;
+
+@Named
+@ViewScoped
+public class CurriculumBean extends AbstractBean {
+
+	private static final long serialVersionUID = -5942086439519787220L;
+
+	private Candidate candidate;
+
+	private Curriculum curriculum;
+
+	private final List<Education> education = new ArrayList<>();
+
+	private final List<JobExperience> jobExperiences = new ArrayList<>();
+
+	private final List<Language> languages = new ArrayList<>();
+
+	private final List<Skill> skills = new ArrayList<>();
+
+	public Candidate getCandidate() {
+		return this.candidate;
+	}
+
+	public Curriculum getCurriculum() {
+		return this.curriculum;
+	}
+
+	public List<Education> getEducation() {
+		return this.education;
+	}
+
+	public List<JobExperience> getJobExperiences() {
+		return this.jobExperiences;
+	}
+
+	public List<Language> getLanguages() {
+		return this.languages;
+	}
+
+	public List<Skill> getSkills() {
+		return this.skills;
+	}
+
+	@PostConstruct
+	public void init() {
+		this.curriculum = this.getValueFromFlash("curriculum");
+		this.candidate = this.getValueFromFlash("candidate");
+		if (this.curriculum == null) {
+			this.curriculum = new Curriculum();
+		}
+		if (this.curriculum.getSkills() == null) {
+			this.curriculum.setSkills(new HashSet<Skill>());
+		}
+		if (this.curriculum.getEducation() == null) {
+			this.curriculum.setEducation(new HashSet<Education>());
+		}
+		if (this.curriculum.getJobExperiences() == null) {
+			this.curriculum.setJobExperiences(new HashSet<JobExperience>());
+		}
+		if (this.curriculum.getLanguages() == null) {
+			this.curriculum.setLanguages(new HashSet<Language>());
+		}
+		this.skills.addAll(this.curriculum.getSkills());
+		this.education.addAll(this.curriculum.getEducation());
+		this.jobExperiences.addAll(this.curriculum.getJobExperiences());
+		this.languages.addAll(this.curriculum.getLanguages());
+	}
+}
