aaa19e7ad37775f77514162dbf60bce946b82435
igochan <igochan@gmail.com>
2018-08-15 17:56:27 +0200

    Final de jobExperienceDialog (creo)

--- ged-web/src/main/java/ged/web/view/curriculum/JobExperienceDialogBean.java
+++ ged-web/src/main/java/ged/web/view/curriculum/JobExperienceDialogBean.java
@@ -1,8 +1,11 @@
 package ged.web.view.curriculum;
 
 import java.time.YearMonth;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -143,5 +146,47 @@ public class JobExperienceDialogBean extends AbstractDialogBean {
 
 	public void setStartYear(final Integer startYear) {
 		this.startYear = startYear;
+	}
+
+	public boolean validateDates(final FacesContext context, final List<UIInput> components, final List<Object> values) {
+		boolean inputStillWorking = false;
+		Integer inputStartMonth = null;
+		Integer inputStartYear = null;
+		Integer inputEndMonth = null;
+		Integer inputEndYear = null;
+
+		final int size = components.size();
+		for (int i = 0; i < size; i++) {
+			final UIInput component = components.get(i);
+			if (component.getId().equals("stillWorking")) {
+				if (values.get(i) == null) {
+					inputStillWorking = false;
+				}
+				else {
+					inputStillWorking = (Boolean) values.get(i);
+				}
+			}
+			else if (component.getId().equals("startMonth")) {
+				inputStartMonth = (Integer) values.get(i);
+			}
+			else if (component.getId().equals("startYear")) {
+				inputStartYear = (Integer) values.get(i);
+			}
+			else if (component.getId().equals("endMonth")) {
+				inputEndMonth = (Integer) values.get(i);
+			}
+			else if (component.getId().equals("endYear")) {
+				inputEndYear = (Integer) values.get(i);
+			}
+		}
+		if (inputStillWorking) {
+			return true;
+		}
+		final YearMonth startDate = this.buildYearMonth(inputStartYear, inputStartMonth);
+		final YearMonth endDate = this.buildYearMonth(inputEndYear, inputEndMonth);
+		if (startDate.isAfter(endDate)) {
+			return false;
+		}
+		return true;
 	}
 }
