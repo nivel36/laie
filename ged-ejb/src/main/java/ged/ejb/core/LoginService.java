113843eae1fe3f2817c5adfe1d3c1dc97af67162
igochan <igochan@gmail.com>
2018-09-11 19:58:58 +0200

    Bug

--- ged-ejb/src/main/java/ged/ejb/core/LoginService.java
+++ ged-ejb/src/main/java/ged/ejb/core/LoginService.java
@@ -1,17 +1,19 @@
 package ged.ejb.core;
 
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.security.auth.login.LoginException;
+import javax.xml.bind.DatatypeConverter;
 
 import ged.ejb.core.action.Action.ActionType;
 import ged.ejb.user.User;
 import ged.ejb.user.UserService;
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Stateless
 public class LoginService {
@@ -20,25 +22,29 @@ public class LoginService {
 	private UserService userService;
 
 	@Audited(action = ActionType.LOGIN)
-	public User saveLastConnection(final String email) {
-		final User user = this.userService.findUserByEmail(email);
-		user.setLastConnection(LocalDateTime.now());
-		return this.userService.save(user);
-	}
-
-	@Audited(action = ActionType.LOGIN)
 	public User login(final String email, final String password) {
 		final User user = this.userService.findUserByEmail(email);
 		try {
-			byte[] hashPassword = MessageDigest.getInstance("SHA-256").digest(password.getBytes(UTF_8));
-			if (!user.getPassword().equals(hashPassword)) {
+			final byte[] hashPassword = MessageDigest.getInstance("SHA-256").digest(password.getBytes(StandardCharsets.UTF_8));
+
+			final char[] hashBase64Password = DatatypeConverter.printBase64Binary(hashPassword).toCharArray();
+
+			if (!Arrays.equals(user.getPassword(), hashBase64Password)) {
 				throw new LoginException();
 			}
 			user.setLastConnection(LocalDateTime.now());
 			return this.userService.save(user);
-		} catch (NoSuchAlgorithmException | LoginException e) {
+		}
+		catch (NoSuchAlgorithmException | LoginException e) {
 			throw new BadLoginException();
 		}
+	}
+
+	@Audited(action = ActionType.LOGIN)
+	public User saveLastConnection(final String email) {
+		final User user = this.userService.findUserByEmail(email);
+		user.setLastConnection(LocalDateTime.now());
+		return this.userService.save(user);
 	}
 
 	public void setUserSerivce(final UserService userSerivce) {
