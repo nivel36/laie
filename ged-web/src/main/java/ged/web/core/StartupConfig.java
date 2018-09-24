e9259d227cf6a63d120cccc3689bcb586b071bd9
igochan <igochan@gmail.com>
2018-09-24 18:58:47 +0200

    0.6.0

--- ged-web/src/main/java/ged/web/core/StartupConfig.java
+++ ged-web/src/main/java/ged/web/core/StartupConfig.java
@@ -1,6 +1,10 @@
 package ged.web.core;
 
+import java.io.IOException;
 import java.lang.invoke.MethodHandles;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.Initialized;
@@ -23,14 +27,34 @@ public class StartupConfig {
 	@Inject
 	private Indexer indexer;
 
-	public void init(@Observes @Initialized(ApplicationScoped.class) final ServletContext context) throws InterruptedException {
-		logger.info("Setting up application");
+	private void create(final String dir) throws IOException {
+		final Path path = Paths.get(dir);
+		if (Files.notExists(path)) {
+			logger.info("Directory {} will be created.", dir);
+			Files.createDirectories(path);
+		}
+	}
+
+	private void createAppDirectories() throws IOException {
+		create("/temp/lucene");
+		create("/temp/files");
+		create("/temp/img");
+	}
+
+	private void indexerInit() throws InterruptedException {
 		try {
 			this.indexer.index();
 		}
 		catch (final InterruptedException e) {
-			StartupConfig.logger.error("Indexer fail", e);
+			logger.error("Indexer fail", e);
 			throw e;
 		}
+	}
+
+	public void init(@Observes @Initialized(ApplicationScoped.class) final ServletContext context) throws InterruptedException, IOException {
+		logger.info("Setting up application");
+		createAppDirectories();
+		indexerInit();
+
 	}
 }
