5d4f4d193557da90ef78200e8f53eaac0da513f2
igochan <igochan@gmail.com>
2018-10-14 21:22:52 +0200

    probando composites

---
+++ ged-web/src/main/java/ged/web/core/component/InputClient.java
@@ -0,0 +1,9 @@
+package ged.web.core.component;
+
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UIInput;
+
+@FacesComponent()
+public class InputClient extends UIInput {
+
+}
