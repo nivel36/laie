5ff66d904a4bf7abaacdf7a6f6ab01eab7c375bc
igochan <igochan@gmail.com>
2018-11-10 12:42:12 +0100

    Simplificado el identityStore

--- ged-ejb/src/main/java/ged/ejb/core/security/GedIdentityStore.java
+++ ged-ejb/src/main/java/ged/ejb/core/security/GedIdentityStore.java
@@ -2,7 +2,6 @@ package ged.ejb.core.security;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.function.Supplier;
 
 import javax.inject.Inject;
 import javax.security.enterprise.credential.CallerOnlyCredential;
@@ -29,22 +28,20 @@ public class GedIdentityStore implements IdentityStore {
 
 	@Override
 	public CredentialValidationResult validate(final Credential credential) {
-		Supplier<User> userSupplier = null;
-
+		final User user;
 		if (credential instanceof UsernamePasswordCredential) {
 			final String email = ((UsernamePasswordCredential) credential).getCaller();
 			final String password = ((UsernamePasswordCredential) credential).getPasswordAsString();
-			userSupplier = () -> loginService.login(email, password);
+			user = loginService.login(email, password);
 		}
 		else if (credential instanceof CallerOnlyCredential) {
 			final String email = ((CallerOnlyCredential) credential).getCaller();
-			userSupplier = () -> userService.findUserByEmail(email);
+			user = userService.findUserByEmail(email);
 		}
 		else {
 			throw new SecurityException();
 		}
 
-		final User user = userSupplier.get();
 		final Set<String> roles = new HashSet<>();
 		roles.add(user.getRole().toString());
 		return new CredentialValidationResult(new GedCallerPrincipal(user), roles);
