a2ccb32c04ff9338d8e7d8885219a85ab225abc7
igochan <igochan@gmail.com>
2018-11-17 11:32:34 +0100

    Corregido el enesimo bug de inputClient

--- ged-web/src/main/java/ged/web/core/view/component/InputClient.java
+++ ged-web/src/main/java/ged/web/core/view/component/InputClient.java
@@ -22,17 +22,17 @@ public class InputClient extends UIInput implements NamingContainer {
 
 	public void clean() {
 		this.input.setValue(null);
-		hideCleanButton();
+		this.hideCleanButton();
 	}
 
 	@Override
 	public void encodeBegin(final FacesContext context) throws IOException {
 		super.encodeBegin(context);
-		processCleanButton();
+		this.processCleanButton();
 	}
 
 	public UICommand getCleanButton() {
-		return cleanButton;
+		return this.cleanButton;
 	}
 
 	@Override
@@ -41,23 +41,23 @@ public class InputClient extends UIInput implements NamingContainer {
 	}
 
 	public UIInput getInput() {
-		return input;
+		return this.input;
 	}
 
 	private void hideCleanButton() {
-		cleanButton.setRendered(false);
+		this.cleanButton.setRendered(false);
 	}
 
 	private boolean isInputValueSet() {
-		return input.getValue() != null;
+		return this.input.getValue() != null;
 	}
 
 	private void processCleanButton() {
-		if (isInputValueSet()) {
-			showCleanButton();
+		if (this.isInputValueSet()) {
+			this.showCleanButton();
 		}
 		else {
-			hideCleanButton();
+			this.hideCleanButton();
 		}
 	}
 
@@ -70,6 +70,6 @@ public class InputClient extends UIInput implements NamingContainer {
 	}
 
 	private void showCleanButton() {
-		cleanButton.setRendered(true);
+		this.cleanButton.setRendered(true);
 	}
 }
