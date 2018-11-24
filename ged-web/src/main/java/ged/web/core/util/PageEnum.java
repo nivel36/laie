e955dafb092bdf0d9f903a5f205f0ee30596f07e
igochan <igochan@gmail.com>
2018-11-24 21:20:15 +0100

    Eliminado el objeto Page antiguo con codigo de navegacion

--- ged-web/src/main/java/ged/web/core/util/PageEnum.java
+++ ged-web/src/main/java/ged/web/core/util/PageEnum.java
@@ -2,14 +2,25 @@ package ged.web.core.util;
 
 public enum PageEnum {
 
-	CANDIDATE("/faces/candidate/candidate"), CANDIDATE_EDIT("/faces/candidate/candidateEdit"), CANDIDATE_SEARCH(
-			"/faces/candidate/candidateSearch"), CANDIDATE_SELECT("/faces/candidate/candidateSelect"), CURRICULUM("/faces/candidate/curriculum"), CLIENT(
-					"/faces/client/client"), CLIENT_SEARCH("/faces/client/clientSearch"), CONTACT("/faces/client/contact"), CONTACT_EDIT(
-							"/faces/client/contactEdit"), INDEX("/faces/index"), JOB_OFFER("/faces/jobOffer/jobOffer"), JOB_OFFER_EDIT(
-									"/faces/jobOffer/jobOfferEdit"), JOB_OFFER_SEARCH("/faces/jobOffer/jobOfferSearch"), LOGIN("/login"), USER(
-											"/faces/user/user"), USER_EDIT("/faces/user/userEdit"), USER_SEARCH("/faces/user/userSearch"), REPORT(
-													"/faces/report/reportSearch"), MAINTENANCE(
-															"/faces/maintenance/maintenanceIndex"), ISABEL("/faces/isabel/isabel");
+	CANDIDATE("/faces/candidate/candidate"), //
+	CANDIDATE_EDIT("/faces/candidate/candidateEdit"), //
+	CANDIDATE_SEARCH("/faces/candidate/candidateSearch"), //
+	CANDIDATE_SELECT("/faces/candidate/candidateSelect"), //
+	CLIENT("/faces/client/client"), //
+	CLIENT_SEARCH("/faces/client/clientSearch"), //
+	CONTACT("/faces/client/contact"), //
+	CONTACT_EDIT("/faces/client/contactEdit"), //
+	CURRICULUM("/faces/candidate/curriculum"), //
+	INDEX("/faces/index"), //
+	ISABEL("/faces/isabel/isabel"), //
+	JOB_OFFER("/faces/jobOffer/jobOffer"), //
+	JOB_OFFER_EDIT("/faces/jobOffer/jobOfferEdit"), //
+	JOB_OFFER_SEARCH("/faces/jobOffer/jobOfferSearch"), LOGIN("/login"), //
+	MAINTENANCE("/faces/maintenance/maintenanceIndex"), //
+	REPORT("/faces/report/reportSearch"), //
+	USER("/faces/user/user"), //
+	USER_EDIT("/faces/user/userEdit"), //
+	USER_SEARCH("/faces/user/userSearch");
 
 	private String url;
 
