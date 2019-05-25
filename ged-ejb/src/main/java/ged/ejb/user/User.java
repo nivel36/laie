cb015bc81256af7d00ea0d6bc7d010dc3a8efdf7
Abel Ferrer <abel.ferrer.jimenez@gmail.com>
2019-05-25 19:50:12 +0200

    Eliminado import

--- ged-ejb/src/main/java/ged/ejb/user/User.java
+++ ged-ejb/src/main/java/ged/ejb/user/User.java
@@ -21,7 +21,6 @@ import javax.validation.constraints.Pattern;
 
 import org.hibernate.search.annotations.Analyze;
 import org.hibernate.search.annotations.Field;
-import org.hibernate.search.annotations.Fields;
 import org.hibernate.search.annotations.Index;
 import org.hibernate.search.annotations.Indexed;
 import org.hibernate.search.annotations.SortableField;
