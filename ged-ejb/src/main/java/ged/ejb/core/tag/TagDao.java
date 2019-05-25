2d3e5c8b242c9f7f3bb38c6e8dd667ac86c85098
Abel Ferrer <abel.ferrer.jimenez@gmail.com>
2019-05-25 21:00:34 +0200

    Arreglado NoResultException

--- ged-ejb/src/main/java/ged/ejb/core/tag/TagDao.java
+++ ged-ejb/src/main/java/ged/ejb/core/tag/TagDao.java
@@ -4,6 +4,8 @@ import static ged.ejb.core.util.Parameters.map;
 
 import java.util.Objects;
 
+import javax.persistence.NoResultException;
+
 import ged.ejb.core.model.AbstractDao;
 import ged.ejb.core.model.Repository;
 
@@ -12,7 +14,11 @@ public class TagDao extends AbstractDao<Tag> {
 
 	public Tag findByLabel(final String label) {
 		Objects.requireNonNull(label, "Label can't be null");
-		return this.getPersistenceFacade().findByQuery(Tag.class, "Tag.findByLabel", map("label", label));
+		try {
+			return this.getPersistenceFacade().findByQuery(Tag.class, "Tag.findByLabel", map("label", label));
+		} catch (final NoResultException e) {
+			return null;
+		}
 	}
 
 	@Override
