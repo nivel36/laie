56e1c292e949ba7f8b2fe73526999f141df3c495
aferrer <igochan@gmail.com>
2019-05-29 09:52:24 +0200

    newEvent skel

--- ged-web/src/main/java/ged/web/core/util/PageEnum.java
+++ ged-web/src/main/java/ged/web/core/util/PageEnum.java
@@ -10,6 +10,7 @@ public enum PageEnum {
 	CANDIDATE_SEARCH("/candidate/search"), //
 	CANDIDATE_SELECT("/candidate/select"), //
 	CALENDAR("/calendar"), //
+	CALENDAR_NEW_EVENT("/calendar/newEvent"), //
 	CHANGE_PASSWORD("/changePassword"), //
 	CLIENT("/client/view"), //
 	CLIENT_ADD("/client/add"), //
