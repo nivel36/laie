a9b567631a4663333a907b2f5630b445a76460dc
aferrer <igochan@gmail.com>
2019-06-21 09:45:33 +0200

    meeting

--- ged-web/src/main/java/ged/web/view/meeting/SearchMeetingView.java
+++ ged-web/src/main/java/ged/web/view/meeting/SearchMeetingView.java
@@ -1,18 +1,11 @@
 package ged.web.view.meeting;
 
-import java.sql.Date;
-import java.time.ZoneOffset;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-
-import org.primefaces.model.DefaultScheduleEvent;
-import org.primefaces.model.LazyScheduleModel;
-import org.primefaces.model.ScheduleEvent;
-import org.primefaces.model.ScheduleModel;
 
 import ged.ejb.core.model.Page;
 import ged.ejb.job.meeting.Meeting;
@@ -30,22 +23,13 @@ public class SearchMeetingView extends AbstractView {
 	@Inject
 	private transient MeetingService meetingService;
 
-	private transient ScheduleModel schedule;
-
-	public ScheduleModel getSchedule() {
-		return schedule;
+	public List<Meeting> getMeetings() {
+		return meetings;
 	}
 
 	@PostConstruct
 	public void init() {
 		meetings = initMeetings();
-		schedule = new LazyScheduleModel();
-		for (Meeting meeting : meetings) {
-			ScheduleEvent event = new DefaultScheduleEvent(meeting.getDescription(),
-					Date.from(meeting.getDatePlanned().toInstant(ZoneOffset.UTC)),
-					Date.from(meeting.getDatePlanned().plusMinutes(30).toInstant(ZoneOffset.UTC)));
-			schedule.addEvent(event);
-		}
 	}
 
 	private List<Meeting> initMeetings() {
