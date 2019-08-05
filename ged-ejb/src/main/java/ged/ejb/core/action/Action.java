1740a23880d43b4b63e39e5b99816adf8b309ee9
Abel Ferrer <abel.ferrer.jimenez@gmail.com>
2019-08-05 09:38:27 +0200

    Varios

--- ged-ejb/src/main/java/ged/ejb/core/action/Action.java
+++ ged-ejb/src/main/java/ged/ejb/core/action/Action.java
@@ -1,5 +1,52 @@
 package ged.ejb.core.action;
 
-public interface Action {
+import java.time.LocalDateTime;
+
+import javax.persistence.MappedSuperclass;
+
+import ged.ejb.core.model.AbstractEntity;
+import ged.ejb.core.model.Ownerable;
+import ged.ejb.user.User;
+
+@MappedSuperclass
+public abstract class AbstractAction extends AbstractEntity {
+
+	private static final long serialVersionUID = -3095037007290696579L;
+
+	private LocalDateTime instant;
+	
+	private String message;
+
+	private User user;
+
+	public AbstractAction() {
+		instant = LocalDateTime.now();
+	}
+
+	public LocalDateTime getInstant() {
+		return instant;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	abstract Ownerable getObject();
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setInstant(LocalDateTime instant) {
+		this.instant = instant;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
 
 }
