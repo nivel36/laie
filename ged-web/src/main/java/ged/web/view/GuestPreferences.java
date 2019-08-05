44f3b5b70bcc47c56ef1a01fb1df0cb9b4f5e35d
Abel Ferrer <abel.ferrer.jimenez@gmail.com>
2019-08-05 14:22:22 +0200

    Bean de preferencias de roma

---
+++ ged-web/src/main/java/ged/web/view/GuestPreferences.java
@@ -0,0 +1,78 @@
+package ged.web.view;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class GuestPreferences implements Serializable {
+
+	private static final long serialVersionUID = 4947141483877255221L;
+
+	private String menuMode = "layout-static";
+
+	private String theme = "blue";
+
+	private String menuColor = "layout-menu-light";
+
+	private String topBarColor = "layout-topbar-blue";
+
+	private String logo = "logo-roma-white";
+
+	private String profileMode = "popup";
+
+	public String getLogo() {
+		return this.logo;
+	}
+
+	public String getMenuColor() {
+		return this.menuColor;
+	}
+
+	public String getMenuMode() {
+		return this.menuMode;
+	}
+
+	public String getProfileMode() {
+		return this.profileMode;
+	}
+
+	public String getTheme() {
+		return this.theme;
+	}
+
+	public String getTopBarColor() {
+		return this.topBarColor;
+	}
+
+	public void setMenuColor(final String menuColor) {
+		this.menuColor = menuColor;
+	}
+
+	public void setMenuMode(final String menuMode) {
+		this.menuMode = menuMode;
+
+		if (this.menuMode.equals("layout-horizontal")) {
+			this.profileMode = "popup";
+		}
+	}
+
+	public void setProfileMode(final String profileMode) {
+		if (this.menuMode.equals("layout-horizontal")) {
+			this.profileMode = "popup";
+		} else {
+			this.profileMode = profileMode;
+		}
+	}
+
+	public void setTheme(final String theme) {
+		this.theme = theme;
+	}
+
+	public void setTopBarColor(final String topBarColor, final String logo) {
+		this.topBarColor = topBarColor;
+		this.logo = logo;
+	}
+}
