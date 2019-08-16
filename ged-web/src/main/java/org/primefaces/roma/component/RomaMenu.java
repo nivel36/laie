6b8a0148be091e87c6ad92caa0ceb13a6d147e75
Abel Ferrer <abel.ferrer.jimenez@gmail.com>
2019-08-16 16:18:07 +0200

    Estilo Roma, preeliminar

---
+++ ged-web/src/main/java/org/primefaces/roma/component/RomaMenu.java
@@ -0,0 +1,129 @@
+package org.primefaces.roma.component;
+
+import javax.faces.component.UIComponent;
+import javax.faces.component.UINamingContainer;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ComponentSystemEvent;
+import javax.faces.event.ComponentSystemEventListener;
+import javax.faces.event.ListenerFor;
+import javax.faces.event.PostAddToViewEvent;
+
+import org.primefaces.component.api.Widget;
+import org.primefaces.component.menu.AbstractMenu;
+import org.primefaces.model.menu.MenuModel;
+
+@ListenerFor(sourceClass = RomaMenu.class, systemEventClass = PostAddToViewEvent.class)
+public class RomaMenu extends AbstractMenu implements Widget, ComponentSystemEventListener {
+	public enum PropertyKeys {
+		widgetVar, model, style, styleClass;
+
+		String toString;
+
+		private PropertyKeys() {
+		}
+
+		private PropertyKeys(final String toString) {
+			this.toString = toString;
+		}
+
+		@Override
+		public String toString() {
+			return this.toString != null ? this.toString : super.toString();
+		}
+	}
+
+	public static final String COMPONENT_TYPE = "org.primefaces.component.RomaMenu";
+	public static final String COMPONENT_FAMILY = "org.primefaces.component";
+	private static final String DEFAULT_RENDERER = "org.primefaces.component.RomaMenuRenderer";
+	private static final String[] LEGACY_RESOURCES = new String[] { "primefaces.css", "jquery/jquery.js",
+			"jquery/jquery-plugins.js", "primefaces.js" };
+
+	private static final String[] MODERN_RESOURCES = new String[] { "components.css", "jquery/jquery.js",
+			"jquery/jquery-plugins.js", "core.js" };
+
+	public RomaMenu() {
+		this.setRendererType("org.primefaces.component.RomaMenuRenderer");
+	}
+
+	@Override
+	public String getFamily() {
+		return "org.primefaces.component";
+	}
+
+	@Override
+	public MenuModel getModel() {
+		return (MenuModel) this.getStateHelper().eval(PropertyKeys.model, (Object) null);
+	}
+
+	public String getStyle() {
+		return (String) this.getStateHelper().eval(PropertyKeys.style, (Object) null);
+	}
+
+	public String getStyleClass() {
+		return (String) this.getStateHelper().eval(PropertyKeys.styleClass, (Object) null);
+	}
+
+	public String getWidgetVar() {
+		return (String) this.getStateHelper().eval(PropertyKeys.widgetVar, (Object) null);
+	}
+
+	@Override
+	public void processEvent(final ComponentSystemEvent event) throws AbortProcessingException {
+		if (event instanceof PostAddToViewEvent) {
+			final FacesContext context = this.getFacesContext();
+			final UIViewRoot root = context.getViewRoot();
+
+			boolean isPrimeConfig;
+			try {
+				isPrimeConfig = Class.forName("org.primefaces.config.PrimeConfiguration") != null;
+			} catch (final ClassNotFoundException var11) {
+				isPrimeConfig = false;
+			}
+
+			final String[] resources = isPrimeConfig ? MODERN_RESOURCES : LEGACY_RESOURCES;
+			final String[] var6 = resources;
+			final int var7 = resources.length;
+
+			for (int var8 = 0; var8 < var7; ++var8) {
+				final String res = var6[var8];
+				final UIComponent component = context.getApplication().createComponent("javax.faces.Output");
+				if (res.endsWith("css")) {
+					component.setRendererType("javax.faces.resource.Stylesheet");
+				} else if (res.endsWith("js")) {
+					component.setRendererType("javax.faces.resource.Script");
+				}
+
+				component.getAttributes().put("library", "primefaces");
+				component.getAttributes().put("name", res);
+				root.addComponentResource(context, component);
+			}
+		}
+	}
+
+	@Override
+	public String resolveWidgetVar() {
+		final FacesContext context = this.getFacesContext();
+		final String userWidgetVar = (String) this.getAttributes().get("widgetVar");
+		return userWidgetVar != null ? userWidgetVar
+				: "widget_"
+						+ this.getClientId(context).replaceAll("-|" + UINamingContainer.getSeparatorChar(context), "_");
+	}
+
+	public void setModel(final MenuModel _model) {
+		this.getStateHelper().put(PropertyKeys.model, _model);
+	}
+
+	public void setStyle(final String _style) {
+		this.getStateHelper().put(PropertyKeys.style, _style);
+	}
+
+	public void setStyleClass(final String _styleClass) {
+		this.getStateHelper().put(PropertyKeys.styleClass, _styleClass);
+	}
+
+	public void setWidgetVar(final String _widgetVar) {
+		this.getStateHelper().put(PropertyKeys.widgetVar, _widgetVar);
+	}
+}
---
+++ ged-web/src/main/java/org/primefaces/roma/component/RomaMenuRenderer.java
@@ -0,0 +1,371 @@
+package org.primefaces.roma.component;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import javax.faces.FacesException;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import org.primefaces.component.api.AjaxSource;
+import org.primefaces.component.api.UIOutcomeTarget;
+import org.primefaces.component.menu.AbstractMenu;
+import org.primefaces.component.menu.BaseMenuRenderer;
+import org.primefaces.component.menuitem.UIMenuItem;
+import org.primefaces.component.submenu.UISubmenu;
+import org.primefaces.expression.SearchExpressionFacade;
+import org.primefaces.model.menu.MenuElement;
+import org.primefaces.model.menu.MenuItem;
+import org.primefaces.model.menu.Separator;
+import org.primefaces.model.menu.Submenu;
+import org.primefaces.util.AjaxRequestBuilder;
+import org.primefaces.util.ComponentTraversalUtils;
+import org.primefaces.util.WidgetBuilder;
+
+public class RomaMenuRenderer extends BaseMenuRenderer {
+	protected void encodeMarkup(FacesContext context, AbstractMenu abstractMenu) throws IOException {
+		RomaMenu menu = (RomaMenu) abstractMenu;
+		ResponseWriter writer = context.getResponseWriter();
+		String style = menu.getStyle();
+		String styleClass = menu.getStyleClass();
+		String defaultStyleClass = "layout-menu";
+		styleClass = styleClass == null ? defaultStyleClass : defaultStyleClass + " " + styleClass;
+		writer.startElement("ul", menu);
+		writer.writeAttribute("id", menu.getClientId(context), "id");
+		writer.writeAttribute("class", styleClass, "styleClass");
+		if (style != null) {
+			writer.writeAttribute("style", style, "style");
+		}
+
+		if (menu.getElementsCount() > 0) {
+			this.encodeElements(context, menu, menu.getElements());
+		}
+
+		writer.endElement("ul");
+	}
+
+	protected void encodeElements(FacesContext context, AbstractMenu menu, List<MenuElement> elements)
+			throws IOException {
+		int size = elements.size();
+
+		for (int i = 0; i < size; ++i) {
+			this.encodeElement(context, menu, (MenuElement) elements.get(i));
+		}
+
+	}
+
+	protected void encodeElement(FacesContext context, AbstractMenu menu, MenuElement element) throws IOException {
+		ResponseWriter writer = context.getResponseWriter();
+		if (element.isRendered()) {
+			String submenuClientId;
+			String style;
+			String styleClass;
+			if (element instanceof MenuItem) {
+				MenuItem menuItem = (MenuItem) element;
+				submenuClientId = menuItem instanceof UIComponent
+						? menuItem.getClientId()
+						: menu.getClientId(context) + "_" + menuItem.getClientId();
+				style = menuItem.getContainerStyle();
+				styleClass = menuItem.getContainerStyleClass();
+				writer.startElement("li", (UIComponent) null);
+				writer.writeAttribute("id", submenuClientId, (String) null);
+				writer.writeAttribute("role", "menuitem", (String) null);
+				if (style != null) {
+					writer.writeAttribute("style", style, (String) null);
+				}
+
+				if (styleClass != null) {
+					writer.writeAttribute("class", styleClass, (String) null);
+				}
+
+				this.encodeMenuItem(context, menu, menuItem);
+				writer.endElement("li");
+			} else if (element instanceof Submenu) {
+				Submenu submenu = (Submenu) element;
+				submenuClientId = submenu instanceof UIComponent
+						? ((UIComponent) submenu).getClientId()
+						: menu.getClientId(context) + "_" + submenu.getId();
+				style = submenu.getStyle();
+				styleClass = submenu.getStyleClass();
+				writer.startElement("li", (UIComponent) null);
+				writer.writeAttribute("id", submenuClientId, (String) null);
+				writer.writeAttribute("role", "menuitem", (String) null);
+				if (style != null) {
+					writer.writeAttribute("style", style, (String) null);
+				}
+
+				if (styleClass != null) {
+					writer.writeAttribute("class", styleClass, (String) null);
+				}
+
+				this.encodeSubmenu(context, menu, submenu);
+				writer.endElement("li");
+			} else if (element instanceof Separator) {
+				this.encodeSeparator(context, (Separator) element);
+			}
+		}
+
+	}
+
+	protected void encodeSubmenu(FacesContext context, AbstractMenu menu, Submenu submenu) throws IOException {
+		ResponseWriter writer = context.getResponseWriter();
+		String icon = submenu.getIcon();
+		String label = submenu.getLabel();
+		int childrenElementsCount = submenu.getElementsCount();
+		writer.startElement("a", (UIComponent) null);
+		writer.writeAttribute("href", "#", (String) null);
+		this.encodeItemIcon(context, icon);
+		if (label != null) {
+			writer.startElement("span", (UIComponent) null);
+			writer.writeAttribute("class", "layout-menuitem-text", (String) null);
+			writer.writeText(label, (String) null);
+			writer.endElement("span");
+			this.encodeToggleIcon(context, submenu, childrenElementsCount);
+			if (submenu instanceof UISubmenu) {
+				this.encodeBadge(context, ((UISubmenu) submenu).getAttributes().get("badge"));
+			}
+		}
+
+		writer.endElement("a");
+		if (label != null) {
+			this.encodeTooltip(context, label);
+		}
+
+		if (childrenElementsCount > 0) {
+			writer.startElement("ul", (UIComponent) null);
+			writer.writeAttribute("role", "menu", (String) null);
+			this.encodeElements(context, menu, submenu.getElements());
+			writer.endElement("ul");
+		}
+
+	}
+
+	protected void encodeItemIcon(FacesContext context, String icon) throws IOException {
+		if (icon != null) {
+			ResponseWriter writer = context.getResponseWriter();
+			writer.startElement("i", (UIComponent) null);
+			if (icon.contains("fa ")) {
+				icon = icon + " fa-fw";
+			}
+
+			writer.writeAttribute("class", icon + " layout-menuitem-icon", (String) null);
+			writer.endElement("i");
+		}
+
+	}
+
+	protected void encodeToggleIcon(FacesContext context, Submenu submenu, int childrenElementsCount)
+			throws IOException {
+		if (childrenElementsCount > 0) {
+			ResponseWriter writer = context.getResponseWriter();
+			writer.startElement("i", (UIComponent) null);
+			writer.writeAttribute("class", "fa fa-fw layout-submenu-toggler fa-angle-down", (String) null);
+			writer.endElement("i");
+		}
+
+	}
+
+	protected void encodeBadge(FacesContext context, Object value) throws IOException {
+		if (value != null) {
+			ResponseWriter writer = context.getResponseWriter();
+			writer.startElement("span", (UIComponent) null);
+			writer.writeAttribute("class", "menuitem-badge", (String) null);
+			writer.writeText(value.toString(), (String) null);
+			writer.endElement("span");
+		}
+
+	}
+
+	protected void encodeSeparator(FacesContext context, Separator separator) throws IOException {
+		ResponseWriter writer = context.getResponseWriter();
+		String style = separator.getStyle();
+		String styleClass = separator.getStyleClass();
+		styleClass = styleClass == null ? "Separator" : "Separator " + styleClass;
+		writer.startElement("li", (UIComponent) null);
+		writer.writeAttribute("class", styleClass, (String) null);
+		if (style != null) {
+			writer.writeAttribute("style", style, (String) null);
+		}
+
+		writer.endElement("li");
+	}
+
+	protected void encodeMenuItem(FacesContext context, AbstractMenu menu, MenuItem menuitem) throws IOException {
+		ResponseWriter writer = context.getResponseWriter();
+		String title = menuitem.getTitle();
+		boolean disabled = menuitem.isDisabled();
+		Object value = menuitem.getValue();
+		String style = menuitem.getStyle();
+		String styleClass = menuitem.getStyleClass();
+		writer.startElement("a", (UIComponent) null);
+		if (title != null) {
+			writer.writeAttribute("title", title, (String) null);
+		}
+
+		if (style != null) {
+			writer.writeAttribute("style", style, (String) null);
+		}
+
+		if (styleClass != null) {
+			writer.writeAttribute("class", styleClass, (String) null);
+		}
+
+		if (disabled) {
+			writer.writeAttribute("href", "#", (String) null);
+			writer.writeAttribute("onclick", "return false;", (String) null);
+		} else {
+			String onclick = menuitem.getOnclick();
+			if (menuitem.getUrl() == null && menuitem.getOutcome() == null) {
+				writer.writeAttribute("href", "#", (String) null);
+				UIComponent form = ComponentTraversalUtils.closestForm(context, menu);
+				if (form == null) {
+					throw new FacesException("MenuItem must be inside a form element");
+				}
+
+				String command;
+				if (menuitem.isDynamic()) {
+					String menuClientId = menu.getClientId(context);
+					Map<String, List<String>> params = menuitem.getParams();
+					if (params == null) {
+						params = new LinkedHashMap();
+					}
+
+					List<String> idParams = new ArrayList();
+					idParams.add(menuitem.getId());
+					((Map) params).put(menuClientId + "_menuid", idParams);
+					command = menuitem.isAjax()
+							? this.createAjaxRequest(context, menu, (AjaxSource) menuitem, form, (Map) params)
+							: this.buildNonAjaxRequest(context, menu, form, menuClientId, (Map) params, true);
+				} else {
+					command = menuitem.isAjax()
+							? this.createAjaxRequest(context, (AjaxSource) menuitem, form)
+							: this.buildNonAjaxRequest(context, (UIComponent) menuitem, form,
+									((UIComponent) menuitem).getClientId(context), true);
+				}
+
+				onclick = onclick == null ? command : onclick + ";" + command;
+			} else {
+				String targetURL = this.getTargetURL(context, (UIOutcomeTarget) menuitem);
+				writer.writeAttribute("href", targetURL, (String) null);
+				if (menuitem.getTarget() != null) {
+					writer.writeAttribute("target", menuitem.getTarget(), (String) null);
+				}
+			}
+
+			if (onclick != null) {
+				writer.writeAttribute("onclick", onclick, (String) null);
+			}
+		}
+
+		this.encodeMenuItemContent(context, menu, menuitem);
+		writer.endElement("a");
+		if (value != null) {
+			this.encodeTooltip(context, value);
+		}
+
+	}
+
+	protected void encodeMenuItemContent(FacesContext context, AbstractMenu menu, MenuItem menuitem)
+			throws IOException {
+		ResponseWriter writer = context.getResponseWriter();
+		String icon = menuitem.getIcon();
+		Object value = menuitem.getValue();
+		if (menuitem instanceof UIMenuItem) {
+			this.encodeBadge(context, ((UIMenuItem) menuitem).getAttributes().get("badge"));
+		}
+
+		this.encodeItemIcon(context, icon);
+		if (value != null) {
+			writer.startElement("span", (UIComponent) null);
+			writer.writeAttribute("class", "layout-menuitem-text", (String) null);
+			writer.writeText(value, "value");
+			writer.endElement("span");
+		}
+
+	}
+
+	protected void encodeTooltip(FacesContext context, Object value) throws IOException {
+		ResponseWriter writer = context.getResponseWriter();
+		writer.startElement("div", (UIComponent) null);
+		writer.writeAttribute("class", "layout-menu-tooltip", (String) null);
+		writer.startElement("div", (UIComponent) null);
+		writer.writeAttribute("class", "layout-menu-tooltip-arrow", (String) null);
+		writer.endElement("div");
+		writer.startElement("div", (UIComponent) null);
+		writer.writeAttribute("class", "layout-menu-tooltip-text", (String) null);
+		writer.writeText(value, (String) null);
+		writer.endElement("div");
+		writer.endElement("div");
+	}
+
+	protected void encodeScript(FacesContext context, AbstractMenu abstractMenu) throws IOException {
+		RomaMenu menu = (RomaMenu) abstractMenu;
+		String clientId = menu.getClientId(context);
+		WidgetBuilder wb = this.getWidgetBuilder(context);
+		wb.init("Roma", menu.resolveWidgetVar(), clientId).finish();
+	}
+
+	protected String createAjaxRequest(FacesContext context, AjaxSource source, UIComponent form) {
+		UIComponent component = (UIComponent) source;
+		String clientId = component.getClientId(context);
+		AjaxRequestBuilder builder = this.getAjaxRequestBuilder();
+		builder.init().source(clientId)
+				.form(SearchExpressionFacade.resolveClientId(context, component, source.getForm()))
+				.process(component, source.getProcess()).update(component, source.getUpdate()).async(source.isAsync())
+				.global(source.isGlobal()).delay(source.getDelay()).timeout(source.getTimeout())
+				.partialSubmit(source.isPartialSubmit(), source.isPartialSubmitSet(), source.getPartialSubmitFilter())
+				.resetValues(source.isResetValues(), source.isResetValuesSet())
+				.ignoreAutoUpdate(source.isIgnoreAutoUpdate()).onstart(source.getOnstart()).onerror(source.getOnerror())
+				.onsuccess(source.getOnsuccess()).oncomplete(source.getOncomplete()).params(component);
+		if (form != null) {
+			builder.form(form.getClientId(context));
+		}
+
+		builder.preventDefault();
+		return builder.build();
+	}
+
+	protected String createAjaxRequest(FacesContext context, AbstractMenu menu, AjaxSource source, UIComponent form,
+			Map<String, List<String>> params) {
+		String clientId = menu.getClientId(context);
+		AjaxRequestBuilder builder = this.getAjaxRequestBuilder();
+		builder.init().source(clientId).process(menu, source.getProcess()).update(menu, source.getUpdate())
+				.async(source.isAsync()).global(source.isGlobal()).delay(source.getDelay()).timeout(source.getTimeout())
+				.partialSubmit(source.isPartialSubmit(), source.isPartialSubmitSet(), source.getPartialSubmitFilter())
+				.resetValues(source.isResetValues(), source.isResetValuesSet())
+				.ignoreAutoUpdate(source.isIgnoreAutoUpdate()).onstart(source.getOnstart()).onerror(source.getOnerror())
+				.onsuccess(source.getOnsuccess()).oncomplete(source.getOncomplete()).params(params);
+		if (form != null) {
+			builder.form(form.getClientId(context));
+		}
+
+		builder.preventDefault();
+		return builder.build();
+	}
+
+	protected AjaxRequestBuilder getAjaxRequestBuilder() {
+		Class rootContext;
+		try {
+			rootContext = Class.forName("org.primefaces.context.PrimeRequestContext");
+		} catch (ClassNotFoundException var8) {
+			try {
+				rootContext = Class.forName("org.primefaces.context.RequestContext");
+			} catch (ClassNotFoundException var7) {
+				throw new RuntimeException(var7);
+			}
+		}
+
+		try {
+			Method method = rootContext.getMethod("getCurrentInstance");
+			Object requestContextInstance = method.invoke((Object) null);
+			method = requestContextInstance.getClass().getMethod("getAjaxRequestBuilder");
+			AjaxRequestBuilder builder = (AjaxRequestBuilder) method.invoke(requestContextInstance);
+			return builder;
+		} catch (Exception var6) {
+			throw new RuntimeException(var6);
+		}
+	}
+}
