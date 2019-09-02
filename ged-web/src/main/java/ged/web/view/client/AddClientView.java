01d9ca895ce13aabc189cf2b90a9d75d01729f20
Abel Ferrer <abel.ferrer.jimenez@gmail.com>
2019-09-02 19:14:26 +0200

    Estilo

--- ged-web/src/main/java/ged/web/view/client/AddClientView.java
+++ ged-web/src/main/java/ged/web/view/client/AddClientView.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import ged.ejb.client.Client;
+import ged.ejb.client.Contact;
 import ged.ejb.core.model.Address;
 
 @Named
@@ -20,11 +21,21 @@ public class AddClientView extends AbstractClientView {
 
 	private static final long serialVersionUID = 1412905869664752048L;
 
+	private Contact contact;
+
+	public void addContact() {
+		this.client.addContact(this.contact);
+	}
+
 	private Client buildNewClient() {
 		final Client newClient = new Client();
 		newClient.setOwner(this.sessionUser.get());
 		newClient.setAddress(new Address());
 		return newClient;
+	}
+
+	public Contact getContact() {
+		return this.contact;
 	}
 
 	@PostConstruct
@@ -37,5 +48,9 @@ public class AddClientView extends AbstractClientView {
 		logger.debug("Create new client action performed");
 		this.client = this.clientService.save(this.client);
 		return this.clientUrl();
+	}
+
+	public void setContact(final Contact contact) {
+		this.contact = contact;
 	}
 }
