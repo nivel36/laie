645171afe704d4529dd20e432c7346e1a30e1cac
abel.ferrer.jimenez <abel.ferrer.jimenez@gmail.com>
2019-11-18 14:13:44 +0100

    Creado portal

--- ged-web/src/main/java/ged/web/core/util/PageEnum.java
+++ ged-web/src/main/java/ged/web/core/util/PageEnum.java
@@ -35,6 +35,7 @@ public enum PageEnum {
 	JOB_EDIT_STATE("/job/editState", true), //
 	JOB_SEARCH("/job/search"), //
 	JOB_SELECT("/job/select"), //
+	PORTAL_JOB("/portal/jobOffer"),
 	LOGIN("/login"), //
 	MAINTENANCE("/maintenance"), //
 	MAINTENANCE_JOB_CANDIDATURE_STATES("/maintenance/jobCandidatureStates"), //
---
+++ ged-web/src/main/java/ged/web/view/portal/JobOfferView.java
@@ -0,0 +1,45 @@
+package ged.web.view.portal;
+
+import java.io.Serializable;
+import java.lang.invoke.MethodHandles;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.omnifaces.cdi.Param;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ged.ejb.job.offer.JobOffer;
+import ged.web.core.IllegalPageStateException;
+
+@Named
+@RequestScoped
+public class JobOfferView implements Serializable {
+
+	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass().getName());
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	@Param(name = "id", required = true)
+	private JobOffer jobOffer;
+
+	public JobOffer getJobOffer() {
+		return this.jobOffer;
+	}
+
+	@PostConstruct
+	public void init() {
+		if (this.jobOffer == null) {
+			throw new IllegalPageStateException();
+		}
+		logger.trace("JobOffer {} init", this.jobOffer);
+	}
+
+	public void setJobOffer(final JobOffer jobOffer) {
+		this.jobOffer = jobOffer;
+	}
+}
