669bbdda77dcd527d84f79bb78001ba5b33363fc
aferrer <aferrer@CC3892.grupogodo.local>
2019-12-25 13:06:26 +0100

    Export a pdf

--- ged-ejb/src/main/java/ged/ejb/core/document/DocumentService.java
+++ ged-ejb/src/main/java/ged/ejb/core/document/DocumentService.java
@@ -1,9 +1,10 @@
 package ged.ejb.core.document;
 
 import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Objects;
 
@@ -12,8 +13,6 @@ import javax.inject.Inject;
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.stream.StreamSource;
@@ -21,7 +20,7 @@ import javax.xml.transform.stream.StreamSource;
 import org.apache.fop.apps.Fop;
 import org.apache.fop.apps.FopFactory;
 import org.apache.fop.apps.MimeConstants;
-import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 import ged.ejb.core.AbstractService;
 import ged.ejb.core.model.AbstractDao;
@@ -33,6 +32,9 @@ public class DocumentService extends AbstractService<Document> {
 	@Inject
 	@Repository
 	private DocumentDao documentDao;
+
+	@Inject
+	private FopFactory fopFactory;
 
 	@Override
 	protected AbstractDao<Document> getDao() {
@@ -48,36 +50,23 @@ public class DocumentService extends AbstractService<Document> {
 		return this.documentDao.findDocumentByName(name);
 	}
 
-	public File export() throws SAXException, IOException, TransformerException {
-
-		// Step 1: Construct a FopFactory by specifying a reference to the configuration
-		// file
-		// (reuse if you plan to render multiple documents!)
-		FopFactory fopFactory = FopFactory.newInstance(new File("C:/Temp/fop.xconf"));
-
-		// Step 2: Set up output stream.
-		// Note: Using BufferedOutputStream for performance reasons (helpful with
-		// FileOutputStreams).
-		try (OutputStream out = new BufferedOutputStream(new FileOutputStream(new File("C:/Temp/myfile.pdf"))))
-
-		{
-			// Step 3: Construct fop with desired output format
-			Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, out);
-
-			// Step 4: Setup JAXP using identity transformer
-			TransformerFactory factory = TransformerFactory.newInstance();
-			Transformer transformer = factory.newTransformer(); // identity transformer
-
-			// Step 5: Setup input and output for XSLT transformation
-			// Setup input stream
-			Source src = new StreamSource(new File("C:/Temp/myfile.fo"));
-
-			// Resulting SAX events (the generated FO) must be piped through to FOP
-			Result res = new SAXResult(fop.getDefaultHandler());
-
-			// Step 6: Start XSLT transformation and FOP processing
-			transformer.transform(src, res);
+	public File export(Document document, String xsltPath) throws Exception {
+		final File tempFile = File.createTempFile("document_", "_pdf");
+		try (final OutputStream out = new BufferedOutputStream(new FileOutputStream(tempFile))) {
+			final Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, out);
+			final TransformerFactory factory = TransformerFactory.newInstance();
+			final File xsltFile = new File(xsltPath);
+			final Source xsltSource = new StreamSource(xsltFile);
+			final Transformer transformer = factory.newTransformer(xsltSource);
+			final String text = document.getText();
+			final byte[] textAsBytes = text.getBytes();
+			try (final InputStream is = new ByteArrayInputStream(textAsBytes)) {
+				final Source src = new StreamSource(is);
+				final DefaultHandler defaultHandler = fop.getDefaultHandler();
+				final Result res = new SAXResult(defaultHandler);
+				transformer.transform(src, res);
+			}
 		}
-		return null;
+		return tempFile;
 	}
 }
--- ged-ejb/src/main/java/ged/ejb/core/util/Resources.java
+++ ged-ejb/src/main/java/ged/ejb/core/util/Resources.java
@@ -3,7 +3,11 @@ package ged.ejb.core.util;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UncheckedIOException;
 import java.lang.invoke.MethodHandles;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Properties;
 
 import javax.annotation.PostConstruct;
@@ -15,6 +19,7 @@ import javax.persistence.PersistenceContext;
 import org.apache.fop.apps.FopFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
 
 public class Resources {
 
@@ -26,25 +31,44 @@ public class Resources {
 
 	private Properties properties;
 
-	private InputStream getConfigFile(String path) {
-		this.properties = new Properties();
-		final ClassLoader cl = Thread.currentThread().getContextClassLoader();
-		return cl.getResourceAsStream(path);
+	private InputStream getFileAsStream(String path) {
+		final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		return classLoader.getResourceAsStream(path);
+	}
+
+	private URL getFileAsUrl(String path) {
+		final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		return classLoader.getResource(path);
 	}
 
 	@PostConstruct
-	public void init() throws IOException {
-		try {
-			this.properties.load(getConfigFile("/config.properties"));
+	public void init() {
+		this.properties = loadProperties();
+	}
+
+	private Properties loadProperties() {
+		final Properties p = new Properties();
+		final String fileName = "/config.properties";
+		try (final InputStream inputStream = getFileAsStream(fileName)) {
+			p.load(inputStream);
 		} catch (IOException e) {
-			logger.error("Config properties file not found");
-			throw e;
+			logger.error("File {} not found", fileName);
+			throw new UncheckedIOException(e);
 		}
+		return p;
 	}
 
 	@Produces
-	public FopFactory fopFactory() throws Exception {
-		return FopFactory.newInstance(new File("C:/Temp/fop.xconf"));
+	public FopFactory fopFactory() {
+		try {
+			final String fileName = "/META-INF/fop.xml";
+			final URL urlFile = this.getFileAsUrl(fileName);
+			final URI uriFile = urlFile.toURI();
+			final File file = new File(uriFile);
+			return FopFactory.newInstance(file);
+		} catch (URISyntaxException | SAXException | IOException e) {
+			throw new IllegalStateException(e);
+		}
 	}
 
 	@Produces
