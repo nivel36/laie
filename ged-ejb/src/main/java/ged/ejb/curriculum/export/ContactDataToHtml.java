334565fec47035bccb159240b0ca99e990616b97
Abel <igochan@gmail.com>
2020-02-28 10:53:49 +0100

    Añadido job profile

--- ged-ejb/src/main/java/ged/ejb/curriculum/export/ContactDataToHtml.java
+++ ged-ejb/src/main/java/ged/ejb/curriculum/export/ContactDataToHtml.java
@@ -6,13 +6,16 @@ public class ContactDataToHtml extends AbstractHtmlPrinter {
 
 	String print(final Candidate candidate, String imagePath) {
 		final StringBuilder sb = new StringBuilder();
-		sb.append(this.openDiv("contact"));
-		sb.append("<h1>Datos de contacto</h1>");
+		sb.append(this.openDiv("name-profile"));
+		sb.append(this.printName(candidate.getFullName()));
+		sb.append(this.printJobProfile(candidate.getJobProfile()));
 		if (candidate.getImageFileName() != null) {
 			sb.append(printPicture(imagePath + "/" + candidate.getImageFileName()));
 		}
+		sb.append(closeDiv());
+		sb.append(this.openDiv("contact"));
+		sb.append("<h1>Datos de contacto</h1>");
 		sb.append(this.openDiv("data"));
-		sb.append(this.printName(candidate.getFullName()));
 		sb.append(this.printEmail(candidate.getEmail()));
 		sb.append(this.printPhone(candidate.getPhoneNumber()));
 		sb.append(this.closeDiv());
@@ -29,11 +32,15 @@ public class ContactDataToHtml extends AbstractHtmlPrinter {
 	}
 
 	String printLabel(final String label) {
-		return this.openDiv("label") + label + this.closeDiv();
+		return this.openSpan("label") + label + this.closeSpan();
 	}
 
 	String printName(final String name) {
 		return this.openDiv("name") + printLabel("Nombre") + name + this.closeDiv();
+	}
+	
+	String printJobProfile(final String jobProfile) {
+		return this.openDiv("jobProfile") + printLabel("Perfil") + jobProfile + this.closeDiv();
 	}
 
 	String printPhone(final String phone) {
