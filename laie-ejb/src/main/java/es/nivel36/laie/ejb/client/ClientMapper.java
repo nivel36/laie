cf8954742dd9369d86e2d05c9a94bd5c1707ff38
Abel Ferrer <aferrer@nivel36.es>
2022-02-04 22:46:23 +0100

    Version 0.9.6
    Actualizada la version de wildlfy a 26.0.1
    Modificado el estilo de las botoneras
    Corregido el mapeador de cliente para que añada direcciones
    Añadido más detalle a la modificacion de la seguridad

--- laie-ejb/src/main/java/es/nivel36/laie/ejb/client/ClientMapper.java
+++ laie-ejb/src/main/java/es/nivel36/laie/ejb/client/ClientMapper.java
@@ -3,6 +3,9 @@ package es.nivel36.laie.ejb.client;
 import java.util.Set;
 
 import es.nivel36.laie.ejb.core.Mapper;
+import es.nivel36.laie.ejb.core.model.Address;
+import es.nivel36.laie.ejb.core.model.AddressDto;
+import es.nivel36.laie.ejb.core.model.AddressMapper;
 import es.nivel36.laie.ejb.user.SimpleUserDto;
 import es.nivel36.laie.ejb.user.User;
 import es.nivel36.laie.ejb.user.UserDto;
@@ -13,6 +16,8 @@ public class ClientMapper implements Mapper<Client, ClientDto> {
 	private ContactMapper contactMapper = new ContactMapper();
 
 	private UserMapper userMapper = new UserMapper();
+	
+	private AddressMapper addressMapper = new AddressMapper();
 
 	@Override
 	public ClientDto map(final Client entity) {
@@ -24,7 +29,14 @@ public class ClientMapper implements Mapper<Client, ClientDto> {
 		dto.setUid(entity.getUid());
 		mapOwner(entity, dto);
 		mapContacts(entity, dto);
+		mapAddress(entity, dto);
 		return dto;
+	}
+	
+	private void mapAddress(final Client entity, final ClientDto dto) {
+		final Address address = entity.getAddress();
+		final AddressDto addressDto = addressMapper.map(address);
+		dto.setAddress(addressDto);
 	}
 
 	private void mapOwner(final Client entity, final ClientDto dto) {
