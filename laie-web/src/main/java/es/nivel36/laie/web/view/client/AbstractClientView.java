07fe340910f71f5a1146c7618dfbaf9536cdb817
Abel Ferrer <aferrer@nivel36.es>
2022-05-19 09:29:57 +0200

    Propietario de cliente

--- laie-web/src/main/java/es/nivel36/laie/web/view/client/AbstractClientView.java
+++ laie-web/src/main/java/es/nivel36/laie/web/view/client/AbstractClientView.java
@@ -1,5 +1,6 @@
 package es.nivel36.laie.web.view.client;
 
+import java.util.List;
 import java.util.Objects;
 
 import javax.inject.Inject;
@@ -8,17 +9,24 @@ import org.omnifaces.cdi.Param;
 
 import es.nivel36.laie.ejb.client.Client;
 import es.nivel36.laie.ejb.client.ClientService;
+import es.nivel36.laie.ejb.core.model.Page;
+import es.nivel36.laie.ejb.user.User;
+import es.nivel36.laie.ejb.user.UserService;
 import es.nivel36.laie.web.core.view.AbstractView;
 
 public class AbstractClientView extends AbstractView {
-	
+
 	private static final long serialVersionUID = -7192538193827969820L;
 
-	@Param
-	protected Client client;
+	protected @Param Client client;
 
-	@Inject
-	protected transient ClientService clientService;
+	protected @Inject transient ClientService clientService;
+	
+	protected @Inject transient UserService userService;
+
+	public List<User> queryOwner(final String query) {
+		return this.userService.search(query, Page.FIRST_TEN_RESULTS).getResultData();
+	}
 
 	public Client getClient() {
 		return this.client;
@@ -31,5 +39,10 @@ public class AbstractClientView extends AbstractView {
 	public void setClientService(final ClientService clientService) {
 		Objects.requireNonNull(clientService);
 		this.clientService = clientService;
+	}
+	
+	public void setUserService(final UserService userService) {
+		Objects.requireNonNull(userService);
+		this.userService = userService;
 	}
 }
