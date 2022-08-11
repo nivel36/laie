860fdb9d0279475ba1ef20e4f3596058997bc5d1
Abel Ferrer <aferrer@nivel36.es>
2022-08-11 17:26:26 +0200

    He olvidado el password

---
+++ laie-web/src/main/java/es/nivel36/laie/web/view/ForgetPasswordView.java
@@ -0,0 +1,56 @@
+package es.nivel36.laie.web.view;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import es.nivel36.laie.ejb.user.User;
+import es.nivel36.laie.ejb.user.UserService;
+
+@Named
+@RequestScoped
+public class ForgetPasswordView implements Serializable {
+
+	private static final long serialVersionUID = 7256830981193689547L;
+
+	private @Inject UserService userService;
+
+	private boolean showBadUsername;
+
+	private boolean showEmailSended;
+
+	private String email;
+
+	public void requestChangePassword() {
+		final User user = userService.findUserByEmail(email);
+		if (user == null) {
+			showBadUsername = true;
+		} else {
+			showEmailSended = true;
+		}
+	}
+
+	public boolean isShowBadUsername() {
+		return showBadUsername;
+	}
+
+	public boolean isShowEmailSended() {
+		return showEmailSended;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setUserService(final UserService userService) {
+		Objects.requireNonNull(userService);
+		this.userService = userService;
+	}
+}
