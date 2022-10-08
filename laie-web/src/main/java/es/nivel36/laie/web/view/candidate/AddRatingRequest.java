428a23160791830ad0dcadc0c0df9cb1f3101fee
Abel Ferrer <aferrer@nivel36.es>
2022-10-09 00:49:44 +0200

    Pantalla de solicitar valoracion

---
+++ laie-web/src/main/java/es/nivel36/laie/web/view/candidate/AddRatingRequest.java
@@ -0,0 +1,73 @@
+package es.nivel36.laie.web.view.candidate;
+
+import java.util.List;
+import java.util.Objects;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.omnifaces.cdi.Param;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import es.nivel36.laie.ejb.candidate.Candidate;
+import es.nivel36.laie.ejb.core.model.Page;
+import es.nivel36.laie.ejb.user.User;
+import es.nivel36.laie.ejb.user.UserService;
+import es.nivel36.laie.web.core.view.AbstractView;
+
+@Named
+@ViewScoped
+public class AddRatingRequest extends AbstractView {
+
+	private static final long serialVersionUID = -5291060183292266892L;
+	
+	private static final Logger logger = LoggerFactory.getLogger(AddRatingRequest.class);
+	
+	private @Param(required = true) Candidate candidate;
+	
+	private User requestTo;
+	
+	private String notes;
+	
+	protected transient @Inject UserService userService;
+	
+	public void send() {
+		logger.debug("Send request ACTION performed");
+	}
+	
+	public List<User> query(final String query) {
+		logger.trace("Search user with the string {}", query);
+		return this.userService.search(query, Page.FIRST_TEN_RESULTS).getResultData();
+	}
+
+	public User getRequestTo() {
+		return requestTo;
+	}
+
+	public void setRequestTo(User requestTo) {
+		this.requestTo = requestTo;
+	}
+
+	public Candidate getCandidate() {
+		return candidate;
+	}
+
+	public void setCandidate(Candidate candidate) {
+		this.candidate = candidate;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(final String notes) {
+		this.notes = notes;
+	}
+
+	public void setUserService(final UserService userService) {
+		Objects.requireNonNull(userService);
+		this.userService = userService;
+	}
+}
